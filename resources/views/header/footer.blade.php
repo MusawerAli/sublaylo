c19223b72e5bc7d9d7d49b24b3a9b7c6af7b9bdf
alirazak2 <alirazak2.bwp@gmail.com>
2020-02-19 22:32:46 +0500

    footer change

--- resources/views/header/footer.blade.php
+++ resources/views/header/footer.blade.php
@@ -3,98 +3,85 @@
 			<!-- start footer -->
 			<footer id="footer" class="footer footer--style-3">
 				<div class="container">
-					<div class="row">
-					<div class >
+                    <div class="footer__item">
+                        <nav id="footer__navigation" class="navigation">
+
+                    <div class="row">
+
+                     <div class="col-sm-4">
+                        <div class="col-auto">
+                            <ul>
+                                <li class="active"><a href="index.html">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                 <li><a href="#">Gallery</a></li>
+                                <li><a href="#">Blog</a></li>
+                                </ul>
+
+                                  <br><br><br>
+
+                                  <address>
+                                      <h5 class="__title">Email</h5>
+                                    <p>
+                                        <a href="mailto:support@agrocompany.com">NCBA&E@gmail.com</a>
+                                    </p>
+                                </address>
+                            </div>
+                         </div>
+
+                        <div class="col-sm-4">
+                            <div class="col-auto offset-lg-1">
+                                <ul>
+                                    <li><a href="#">Partners</a></li>
+                                    <li><a href="#">Customer Service</a></li>
+                                    <li><a href="#">Vegetables</a></li>
+                                    <li><a href="#">Fruits</a></li>
+
+                                </ul>
+                                <br><br><br>
+                                <address>
+
+                                    <h5 class="__title">Phone</h5>
+
+                                    <p>
+                                        +92336-513516<br>+92300-9086949
+                                    </p>
+                                  </address>
+
+                            </div>
+                            </div>
 
 
-					</div>
+                            <div class="col-sm-4">
+                                <div class="col-auto">
+                                    <ul>
+                                        <li><a href="#">Contacts</a></li>
+                                        <li><a href="#">Organic Food</a></li>
+                                    <li><a href="#">Privacy policy</a></li>
+                                        <li><a href="#">Customer Products</a></li>
+
+                                    </ul>
+                                    <br><br>
+                                    <h5 class="w3-text-yellow w3-small">Follow Us</h5>
+                                    <div class="social-btns">
+                                        <a href="#"><i class="fontello-twitter"></i></a>
+                                        <a href="#"><i class="fontello-facebook"></i></a>
+                                        <a href="#"><i class="fontello-linkedin-squared"></i></a>
+                                        <a href="#"><i class="fontello-instagram"></i></a>
+                                    </div>
 
 
+                                </div>
+                                </div>
+
+                     </div>
+                </nav>
+            </div>
 
 
-						<div class="col-12 col-md-7">
-							<div class="row">
-							<div class="col-12 col-sm-4 col-md-4 col-lg-3">
-									<div class="footer__item">
-										<a class="site-logo" href="index.html">
-											<img class="img-fluid  lazy" src="img/blank.gif" data-src="img/site_logo.png" alt="demo" />
-										</a>
-									</div>
-								</div>
-
-								<div class="col-12 col-sm">
-									<div class="footer__item">
-										<nav id="footer__navigation" class="navigation">
-											<div class="row justify-content-sm-center">
-												<div class="col-auto">
-													<ul>
-														<li class="active"><a href="index.html">Home</a></li>
-														<li><a href="#">About</a></li>
-
-														<li><a href="#">Gallery</a></li>
-														<li><a href="#">Blog</a></li>
-														<li><a href="#">Contacts</a></li>
-													</ul>
-												</div>
-
-												<div class="col-auto offset-lg-1">
-													<ul>
-														<li><a href="#">Partners</a></li>
-														<li><a href="#">Customer Service</a></li>
-														<li><a href="#">Vegetables</a></li>
-														<li><a href="#">Fruits</a></li>
-														<li><a href="#">Organic Food</a></li>
-														<li><a href="#">Privacy policy</a></li>
-													</ul>
-												</div>
-											</div>
-										</nav>
-									</div>
-								</div>
-
-								<div class="col-12 my-lg-3"></div>
-							</div>
-
-							<div class="row align-items-end justify-content-between">
-								<div class="col-12 col-lg-9">
-									<div class="footer__item">
-										<address>
-											<div class="row">
-												<div class="col-12 col-sm-6 mb-4 mb-sm-0">
-													<h5 class="__title">email</h5>
-
-													<p>
-														<a href="mailto:support@agrocompany.com">support@agrocompany.com</a>
-													</p>
-												</div>
-
-												<div class="col-12 col-sm-6">
-													<h5 class="__title">Phone</h5>
-
-													<p>
-														+1 (234) 56789,  +1 987 654 3210
-													</p>
-												</div>
-											</div>
-										</address>
-									</div>
-								</div>
-
-								<div class="col-12 col-lg-auto">
-									<div class="footer__item">
-										<div class="social-btns">
-											<a href="#"><i class="fontello-twitter"></i></a>
-											<a href="#"><i class="fontello-facebook"></i></a>
-											<a href="#"><i class="fontello-linkedin-squared"></i></a>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<div class="row justify-content-lg-between copyright">
+            <div class="row justify-content-lg-between copyright">
 								<div class="col-12 col-lg">
 									<div class="footer__item">
-										<span class="__copy">© 2019, AgroTheme by <a class="__dev" href="https://themeforest.net/user/artureanec" target="_blank">Artureanec</a> | <a href="#">Privacy Policy</a> | <a href="#">Sitemap</a></span>
+										<span class="__copy">© 2020,K-2 Software House <a class="__dev" href="https://themeforest.net/user/artureanec" target="_blank">Artureanec</a> | <a href="#">Privacy Policy</a> | <a href="#">Sitemap</a></span>
 									</div>
 								</div>
 
@@ -105,8 +92,7 @@
 								</div>
 							</div>
 						</div>
-					</div>
-				</div>
+
 
 				{{-- <div class="map-container">
 					<!-- this is demo key "AIzaSyBXQROV5YMCERGIIuwxrmaZbBl_Wm4Dy5U" -->
